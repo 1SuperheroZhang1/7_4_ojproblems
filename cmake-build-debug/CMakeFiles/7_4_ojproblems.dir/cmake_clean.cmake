ba0158ad669163c5fd7d2725fae4bc020749a01e
zhangcy <2304417358@qq.com>
2023-01-23 22:53:36 +0800

    C语言督学营 - 7.4 oj题目

---
+++ cmake-build-debug/CMakeFiles/7_4_ojproblems.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "7_4_ojproblems.exe"
+  "7_4_ojproblems.exe.manifest"
+  "7_4_ojproblems.pdb"
+  "CMakeFiles/7_4_ojproblems.dir/main.c.obj"
+  "lib7_4_ojproblems.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/7_4_ojproblems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
